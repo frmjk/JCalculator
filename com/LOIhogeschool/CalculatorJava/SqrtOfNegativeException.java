f89f9a0ac737bea9f3ad11a3719ae5e3491d5e45
Fer Mujica <fer.mujca@gmail.com>
2013-09-30 06:08:38 +0200

    First Commit
    First Version

---
+++ com/LOIhogeschool/CalculatorJava/SqrtOfNegativeException.java
@@ -0,0 +1,21 @@
+package com.LOIhogeschool.CalculatorJava;
+
+/**
+ * <p>Copyright: This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.<br>
+ * 2013.</p>
+ * 
+ * @author FJ Mujica
+ * @version: 1.0
+ *
+ */
+
+/**
+ * This is an exception that is triggered when a square root of a negative number is attempted.
+ */
+
+public class SqrtOfNegativeException extends Exception {
+	public SqrtOfNegativeException() {
+		super();
+	}
+	
+}
